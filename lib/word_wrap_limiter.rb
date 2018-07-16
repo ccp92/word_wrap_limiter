b7ae27884253ab4145734d3dcd79699a3b7d5a91
Christopher Parsons <mail@christopherparsons.me>
2018-07-16 14:16:51 +0100

    Word wrap function completed

--- lib/word_wrap_limiter.rb
+++ lib/word_wrap_limiter.rb
@@ -1,7 +1,19 @@
 require "word_wrap_limiter/version"
 
 module WordWrapLimiter
-  def self.wrap(string, line_length)
-    string
+  def self.wrap(string, column_length)
+    return string if string.length <= column_length
+    break_point = string[0...column_length].rindex(' ') || column_length
+    if has_space?(string, column_length)
+      string[0...break_point] + "\n" + wrap(string[break_point + 1..-1], column_length)
+    else
+      string[0...break_point] + "\n" + wrap(string[break_point..-1], column_length)
+    end
+  end
+
+  private
+
+  def self.has_space?(string, column_length)
+    string[0...column_length].index(' ') != nil || string[column_length] == " "
   end
 end
--- spec/word_wrap_limiter_spec.rb
+++ spec/word_wrap_limiter_spec.rb
@@ -3,11 +3,41 @@ RSpec.describe WordWrapLimiter do
     expect(WordWrapLimiter::VERSION).not_to be nil
   end
 
-  it 'does not wrap an empty string onto a new line' do
-    expect(WordWrapLimiter.wrap('', 0)).to eq('')
+  it 'does not wrap an empty sting onto a new line' do
+    word_wrap('', 1, '')
   end
 
-  it 'does not wrap a string below the line length on a new line' do
-    expect(WordWrapLimiter.wrap('word', 6)).to eq('word')
+  it 'does not wrap a string that is shorter than column length' do
+    word_wrap('word', 6, 'word')
+  end
+
+  it 'wraps a string longer than the column length' do
+    word_wrap('word', 2, "wo\nrd")
+  end
+
+  it 'wraps a string onto multiple lines' do
+    word_wrap('longer', 2, "lo\nng\ner")
+  end
+
+  it 'wraps on a space if available' do
+    word_wrap('word word', 5, "word\nword")
+  end
+
+  it 'wraps on the space when column length is past the space' do
+    word_wrap('word word', 6, "word\nword")
+  end
+
+  it 'wraps on spaces and words' do
+    word_wrap('word word', 3, "wor\nd\nwor\nd")
+  end
+
+  it 'wraps just before the space' do
+    word_wrap('word word', 4, "word\nword")
+  end
+
+  private
+
+  def word_wrap(string, line_length, output)
+    expect(WordWrapLimiter.wrap(string, line_length)).to eq(output)
   end
 end
