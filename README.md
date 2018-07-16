f159e9eb51daa28a141c20120bbeba27f0bcc3d7
Christopher Parsons <mail@christopherparsons.me>
2018-07-16 16:48:08 +0100

    Stylistic and README updates

--- README.md
+++ README.md
@@ -20,13 +20,18 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+Example usage in ruby file called from the terminal:
 
-## Development
+require 'word_wrap_limiter'
 
-After checking out the repo, run `bin/setup` to install dependencies. Then, run `rake spec` to run the tests. You can also run `bin/console` for an interactive prompt that will allow you to experiment.
+p 'Enter a string'
+input = gets
 
-To install this gem onto your local machine, run `bundle exec rake install`. To release a new version, update the version number in `version.rb`, and then run `bundle exec rake release`, which will create a git tag for the version, push git commits and tags, and push the `.gem` file to [rubygems.org](https://rubygems.org).
+p 'Enter a column width'
+column_width = gets
+
+result = WordWrapLimiter.wrap(input, column_width.to_i)
+puts result
 
 ## Contributing
 
--- lib/word_wrap_limiter.rb
+++ lib/word_wrap_limiter.rb
@@ -1,19 +1,19 @@
-require "word_wrap_limiter/version"
+# frozen_String_literal: true
+
+require 'word_wrap_limiter/version'
 
 module WordWrapLimiter
-  def self.wrap(string, column_length)
-    return string if string.length <= column_length
-    break_point = string[0...column_length].rindex(' ') || column_length
-    if has_space?(string, column_length)
-      string[0...break_point] + "\n" + wrap(string[break_point + 1..-1], column_length)
+  def self.wrap(string, width)
+    return string if string.length <= width
+    break_point = string[0...width].rindex(' ') || width
+    if space?(string, width)
+      "#{string[0...break_point]}\n#{wrap(string[break_point + 1..-1], width)}"
     else
-      string[0...break_point] + "\n" + wrap(string[break_point..-1], column_length)
+      "#{string[0...break_point]}\n#{wrap(string[break_point..-1], width)}"
     end
   end
 
-  private
-
-  def self.has_space?(string, column_length)
-    string[0...column_length].index(' ') != nil || string[column_length] == " "
+  def self.space?(string, width)
+    !string[0...width].index(' ').nil? || string[width] == ' '
   end
 end
